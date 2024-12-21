9632bd53b6676c8c1e4b03954cd737b3678af0d9
DavidePedrotti <davidepedrotti5@gmail.com>
2024-12-21 14:58:19 +0000

    Fix: now only h6 and h10 can communicate with h9 regardless of the slice

--- topologies/first_topology/utils.py
+++ topologies/first_topology/utils.py
@@ -26,36 +26,36 @@ def get_port (src: str, dst: str) -> int:
 
     link_mapping = {
         "s1": {
-            "h1": 3, 
+            "h1": 3,
             "h10": 4,
-            "s2": 1,  
-            "s4": 2,  
+            "s2": 1,
+            "s4": 2,
         },
         "s2": {
-            "h5": 4,  
-            "s1": 1, 
-            "s3": 2, 
-            "s4": 3, 
+            "h5": 4,
+            "s1": 1,
+            "s3": 2,
+            "s4": 3,
         },
         "s3": {
-            "h2": 3,  
-            "h3": 4, 
-            "h4": 5,  
-            "s2": 1,  
-            "s4": 2,  
+            "h2": 3,
+            "h3": 4,
+            "h4": 5,
+            "s2": 1,
+            "s4": 2,
         },
         "s4": {
             "h6": 2,
             "h7": 3,
-            "h8": 4, 
-            "s1": 1,  
-            "s2": 2, 
-            "s3": 3,  
+            "h8": 4,
+            "s1": 1,
+            "s2": 2,
+            "s3": 3,
             "s5": 4,
         },
         "s5": {
-            "h6": 2,  
-            "h7": 3,  
+            "h6": 2,
+            "h7": 3,
             "h8": 4,
             "h9": 5,
             "s4": 1,
@@ -65,9 +65,72 @@ def get_port (src: str, dst: str) -> int:
 
 def generate_link_entries(src_ip, dst_ips, switch, ports):
     return {src_ip: [{dst_ip: get_port(switch, port)} for dst_ip, port in zip(dst_ips, ports)]}
-    
+
 def slice_to_port(scenario = 0):
-    if scenario == 1:
+    if scenario == 0:
+        return {
+            get_dpid("s1"): {
+                **generate_link_entries(get_IP_address("h10"), [get_IP_address("h9")], "s1", ["s4"]),
+                **generate_link_entries(get_IP_address("h9"), [get_IP_address("h10")], "s1", ["h10"]),
+            },
+            get_dpid("s4"): {
+                **generate_link_entries(get_IP_address("h10"), [get_IP_address("h9")], "s4", ["s5"]),
+                **generate_link_entries(get_IP_address("h9"), [get_IP_address("h10")], "s4", ["s1"]),
+            },
+            get_dpid("s5"): {
+                **generate_link_entries(get_IP_address("h10"), [get_IP_address("h9")], "s5", ["h9"]),
+                **generate_link_entries(get_IP_address("h9"), [get_IP_address("h10")], "s5", ["s4"]),
+            },
+        }
+    elif scenario == 1:
+        return {
+            get_dpid("s1"): {
+                **generate_link_entries(get_IP_address("h2"), [get_IP_address("h10")], "s1", ["h10"]),
+                **generate_link_entries(get_IP_address("h3"), [get_IP_address("h10")], "s1", ["h10"]),
+                **generate_link_entries(get_IP_address("h4"), [get_IP_address("h10")], "s1", ["h10"]),
+                **generate_link_entries(get_IP_address("h5"), [get_IP_address("h10")], "s1", ["h10"]),
+                **generate_link_entries(get_IP_address("h6"), [get_IP_address("h10")], "s1", ["h10"]),
+                **generate_link_entries(get_IP_address("h9"), [get_IP_address("h10")], "s1", ["h10"]),
+                **generate_link_entries(get_IP_address("h10"), [get_IP_address("h2"), get_IP_address("h3"), get_IP_address("h4"), get_IP_address("h5"), get_IP_address("h6"), get_IP_address("h9")], "s1", ["s4"] * 6),
+            },
+            get_dpid("s2"): {
+                **generate_link_entries(get_IP_address("h2"), [get_IP_address("h5")], "s2", ["h5"]),
+                **generate_link_entries(get_IP_address("h3"), [get_IP_address("h5")], "s2", ["h5"]),
+                **generate_link_entries(get_IP_address("h4"), [get_IP_address("h5")], "s2", ["h5"]),
+                **generate_link_entries(get_IP_address("h5"), [get_IP_address("h2"), get_IP_address("h3"), get_IP_address("h4"), get_IP_address("h6"), get_IP_address("h10")], "s2", ["s4"] * 5),
+                **generate_link_entries(get_IP_address("h6"), [get_IP_address("h5")], "s2", ["h5"]),
+                **generate_link_entries(get_IP_address("h10"), [get_IP_address("h5")], "s2", ["h5"]),
+            },
+            get_dpid("s3"): {
+                **generate_link_entries(get_IP_address("h2"), [get_IP_address("h3"), get_IP_address("h4"), get_IP_address("h5"), get_IP_address("h6"), get_IP_address("h10")], "s3", ["h3", "h4"] + ["s4"] * 3),
+                **generate_link_entries(get_IP_address("h3"), [get_IP_address("h2"), get_IP_address("h4"), get_IP_address("h5"), get_IP_address("h6"), get_IP_address("h10")], "s3", ["h2", "h4"] + ["s4"] * 3),
+                **generate_link_entries(get_IP_address("h4"), [get_IP_address("h2"), get_IP_address("h3"), get_IP_address("h5"), get_IP_address("h6"), get_IP_address("h10")], "s3", ["h2", "h3"] + ["s4"] * 3),
+                **generate_link_entries(get_IP_address("h5"), [get_IP_address("h2"), get_IP_address("h3"), get_IP_address("h4")], "s3", ["h2", "h3", "h4"]),
+                **generate_link_entries(get_IP_address("h6"), [get_IP_address("h2"), get_IP_address("h3"), get_IP_address("h4")], "s3", ["h2", "h3", "h4"]),
+                **generate_link_entries(get_IP_address("h10"), [get_IP_address("h2"), get_IP_address("h3"), get_IP_address("h4")], "s3", ["h2", "h3", "h4"]),
+            },
+            get_dpid("s4"): {
+                **generate_link_entries(get_IP_address("h2"), [get_IP_address("h5"), get_IP_address("h6"), get_IP_address("h10")], "s4", ["s2", "s5", "s1"]),
+                **generate_link_entries(get_IP_address("h3"), [get_IP_address("h5"), get_IP_address("h6"), get_IP_address("h10")], "s4", ["s2", "s5", "s1"]),
+                **generate_link_entries(get_IP_address("h4"), [get_IP_address("h5"), get_IP_address("h6"), get_IP_address("h10")], "s4", ["s2", "s5", "s1"]),
+                **generate_link_entries(get_IP_address("h5"), [get_IP_address("h2"), get_IP_address("h3"), get_IP_address("h4"), get_IP_address("h6"), get_IP_address("h10")], "s4", ["s3"] * 3 + ["s5", "s1"]),
+                **generate_link_entries(get_IP_address("h6"), [get_IP_address("h2"), get_IP_address("h3"), get_IP_address("h4"), get_IP_address("h5"), get_IP_address("h10")], "s4", ["s3"] * 3 + ["s2", "s1"]),
+                **generate_link_entries(get_IP_address("h9"), [get_IP_address("h10")], "s4", ["s1"]),
+                **generate_link_entries(get_IP_address("h10"), [get_IP_address("h2"), get_IP_address("h3"), get_IP_address("h4"), get_IP_address("h5"), get_IP_address("h6"), get_IP_address("h9")], "s4", ["s3"] * 3 + ["s2", "s5", "s5"]),
+            },
+            get_dpid("s5"): {
+                **generate_link_entries(get_IP_address("h2"), [get_IP_address("h6")], "s5", ["h6"]),
+                **generate_link_entries(get_IP_address("h3"), [get_IP_address("h6")], "s5", ["h6"]),
+                **generate_link_entries(get_IP_address("h4"), [get_IP_address("h6")], "s5", ["h6"]),
+                **generate_link_entries(get_IP_address("h5"), [get_IP_address("h6")], "s5", ["h6"]),
+                **generate_link_entries(get_IP_address("h6"), [get_IP_address("h2"), get_IP_address("h3"), get_IP_address("h4"), get_IP_address("h5"), get_IP_address("h7"), get_IP_address("h8"), get_IP_address("h9"), get_IP_address("h10")], "s5", ["s4"] * 4 + ["h7", "h8", "h9", "s4"]),
+                **generate_link_entries(get_IP_address("h7"), [get_IP_address("h6")], "s5", ["h6"]),
+                **generate_link_entries(get_IP_address("h8"), [get_IP_address("h6")], "s5", ["h6"]),
+                **generate_link_entries(get_IP_address("h9"), [get_IP_address("h6"), get_IP_address("h10")], "s5", ["h6", "s4"]),
+                **generate_link_entries(get_IP_address("h10"), [get_IP_address("h6"), get_IP_address("h9")], "s5", ["h6", "h9"]),
+            },
+        }
+    elif scenario == 2:
         return {
             get_dpid("s1"): {
                 **generate_link_entries(get_IP_address("h10"), [get_IP_address("h9"), get_IP_address("h6")], "s1", ["s4", "s4"]),
@@ -102,7 +165,7 @@ def slice_to_port(scenario = 0):
                 **generate_link_entries(get_IP_address("h2"), [get_IP_address("h1")], "s2", ["s1"]),
                 **generate_link_entries(get_IP_address("h3"), [get_IP_address("h1")], "s2", ["s1"]),
                 **generate_link_entries(get_IP_address("h4"), [get_IP_address("h1")], "s2", ["s1"]),
-                **generate_link_entries(get_IP_address("h5"), [get_IP_address("h1")], "s2", ["s1"]),               
+                **generate_link_entries(get_IP_address("h5"), [get_IP_address("h1")], "s2", ["s1"]),
             },
             get_dpid("s3"): {
                 **generate_link_entries(get_IP_address("h1"), [get_IP_address("h2"), get_IP_address("h3"), get_IP_address("h4")], "s3", ["h2", "h3", "h4"]),
@@ -121,70 +184,5 @@ def slice_to_port(scenario = 0):
                 **generate_link_entries(get_IP_address("h6"), [get_IP_address("h1"), get_IP_address("h9"), get_IP_address("h10")], "s5", ["s4", "h9", "s4"]),
                 **generate_link_entries(get_IP_address("h9"), [get_IP_address("h1"), get_IP_address("h6"), get_IP_address("h10")], "s5", ["s4", "h6", "s4"]),
                 **generate_link_entries(get_IP_address("h10"), [get_IP_address("h6"), get_IP_address("h9")], "s5", ["h6", "h9"]),
-            },
-        }
-    elif scenario == 2:
-        return {
-            get_dpid("s1"): {
-                **generate_link_entries(get_IP_address("h2"), [get_IP_address("h10")], "s1", ["h10"]),
-                **generate_link_entries(get_IP_address("h3"), [get_IP_address("h10")], "s1", ["h10"]),
-                **generate_link_entries(get_IP_address("h4"), [get_IP_address("h10")], "s1", ["h10"]),
-                **generate_link_entries(get_IP_address("h5"), [get_IP_address("h10")], "s1", ["h10"]),
-                **generate_link_entries(get_IP_address("h6"), [get_IP_address("h10")], "s1", ["h10"]),
-                **generate_link_entries(get_IP_address("h9"), [get_IP_address("h10")], "s1", ["h10"]),
-                **generate_link_entries(get_IP_address("h10"), [get_IP_address("h2"), get_IP_address("h3"), get_IP_address("h4"), get_IP_address("h5"), get_IP_address("h6"), get_IP_address("h9")], "s1", ["s4"] * 6),
-            },
-            get_dpid("s2"): {
-                **generate_link_entries(get_IP_address("h2"), [get_IP_address("h5")], "s2", ["h5"]),
-                **generate_link_entries(get_IP_address("h3"), [get_IP_address("h5")], "s2", ["h5"]),
-                **generate_link_entries(get_IP_address("h4"), [get_IP_address("h5")], "s2", ["h5"]),
-                **generate_link_entries(get_IP_address("h5"), [get_IP_address("h2"), get_IP_address("h3"), get_IP_address("h4"), get_IP_address("h6"), get_IP_address("h9"), get_IP_address("h10")], "s2", ["s4"] * 6),
-                **generate_link_entries(get_IP_address("h6"), [get_IP_address("h5")], "s2", ["h5"]),
-                **generate_link_entries(get_IP_address("h9"), [get_IP_address("h5")], "s2", ["h5"]),  
-                **generate_link_entries(get_IP_address("h10"), [get_IP_address("h5")], "s2", ["h5"]),                            
-            },
-            get_dpid("s3"): {
-                **generate_link_entries(get_IP_address("h2"), [get_IP_address("h3"), get_IP_address("h4"), get_IP_address("h5"), get_IP_address("h6"), get_IP_address("h9"), get_IP_address("h10")], "s3", ["h3", "h4"] + ["s4"] * 4),
-                **generate_link_entries(get_IP_address("h3"), [get_IP_address("h2"), get_IP_address("h4"), get_IP_address("h5"), get_IP_address("h6"), get_IP_address("h9"), get_IP_address("h10")], "s3", ["h2", "h4"] + ["s4"] * 4),
-                **generate_link_entries(get_IP_address("h4"), [get_IP_address("h2"), get_IP_address("h3"), get_IP_address("h5"), get_IP_address("h6"), get_IP_address("h9"), get_IP_address("h10")], "s3", ["h2", "h3"] + ["s4"] * 4),
-                **generate_link_entries(get_IP_address("h5"), [get_IP_address("h2"), get_IP_address("h3"), get_IP_address("h4")], "s3", ["h2", "h3", "h4"]),
-                **generate_link_entries(get_IP_address("h6"), [get_IP_address("h2"), get_IP_address("h3"), get_IP_address("h4")], "s3", ["h2", "h3", "h4"]),    
-                **generate_link_entries(get_IP_address("h9"), [get_IP_address("h2"), get_IP_address("h3"), get_IP_address("h4")], "s3", ["h2", "h3", "h4"]),
-                **generate_link_entries(get_IP_address("h10"), [get_IP_address("h2"), get_IP_address("h3"), get_IP_address("h4")], "s3", ["h2", "h3", "h4"]),
-            },
-            get_dpid("s4"): {
-                **generate_link_entries(get_IP_address("h2"), [get_IP_address("h5"), get_IP_address("h6"), get_IP_address("h9"), get_IP_address("h10")], "s4", ["s2", "s5", "s5", "s1"]),
-                **generate_link_entries(get_IP_address("h3"), [get_IP_address("h5"), get_IP_address("h6"), get_IP_address("h9"), get_IP_address("h10")], "s4", ["s2", "s5", "s5", "s1"]),
-                **generate_link_entries(get_IP_address("h4"), [get_IP_address("h5"), get_IP_address("h6"), get_IP_address("h9"), get_IP_address("h10")], "s4", ["s2", "s5", "s5", "s1"]),
-                **generate_link_entries(get_IP_address("h5"), [get_IP_address("h2"), get_IP_address("h3"), get_IP_address("h4"), get_IP_address("h6"), get_IP_address("h9"), get_IP_address("h10")], "s4", ["s3"] * 3 + ["s5", "s5", "s1"]),
-                **generate_link_entries(get_IP_address("h6"), [get_IP_address("h2"), get_IP_address("h3"), get_IP_address("h4"), get_IP_address("h5"), get_IP_address("h10")], "s4", ["s3"] * 3 + ["s2", "s1"]),    
-                **generate_link_entries(get_IP_address("h9"), [get_IP_address("h2"), get_IP_address("h3"), get_IP_address("h4"), get_IP_address("h5"), get_IP_address("h10")], "s4", ["s3"] * 3 + ["s2", "s1"]), 
-                **generate_link_entries(get_IP_address("h10"), [get_IP_address("h2"), get_IP_address("h3"), get_IP_address("h4"), get_IP_address("h5"), get_IP_address("h6"), get_IP_address("h9")], "s4", ["s3"] * 3 + ["s2", "s5", "s5"]), 
-            },
-            get_dpid("s5"): {
-                **generate_link_entries(get_IP_address("h2"), [get_IP_address("h6"), get_IP_address("h9")], "s5", ["h6", "h9"]),
-                **generate_link_entries(get_IP_address("h3"), [get_IP_address("h6"), get_IP_address("h9")], "s5", ["h6", "h9"]),
-                **generate_link_entries(get_IP_address("h4"), [get_IP_address("h6"), get_IP_address("h9")], "s5", ["h6", "h9"]),
-                **generate_link_entries(get_IP_address("h5"), [get_IP_address("h6"), get_IP_address("h9")], "s5", ["h6", "h9"]),
-                **generate_link_entries(get_IP_address("h6"), [get_IP_address("h2"), get_IP_address("h3"), get_IP_address("h4"), get_IP_address("h5"), get_IP_address("h7"), get_IP_address("h8"), get_IP_address("h9"), get_IP_address("h10")], "s5", ["s4"] * 4 + ["h7", "h8", "h9", "s4"]),
-                **generate_link_entries(get_IP_address("h7"), [get_IP_address("h6")], "s5", ["h6"]),
-                **generate_link_entries(get_IP_address("h8"), [get_IP_address("h6")], "s5", ["h6"]),
-                **generate_link_entries(get_IP_address("h9"), [get_IP_address("h2"), get_IP_address("h3"), get_IP_address("h4"), get_IP_address("h5"), get_IP_address("h6"), get_IP_address("h10")], "s5", ["s4"] * 4 + ["h6", "s4"]),
-                **generate_link_entries(get_IP_address("h10"), [get_IP_address("h6"), get_IP_address("h9")], "s5", ["h6", "h9"]),
-            },
-        }
-    elif scenario == 0:
-        return {
-            get_dpid("s1"): {
-                **generate_link_entries(get_IP_address("h10"), [get_IP_address("h9")], "s1", ["s4"]),
-                **generate_link_entries(get_IP_address("h9"), [get_IP_address("h10")], "s1", ["h10"]),
-            },
-            get_dpid("s4"): {
-                **generate_link_entries(get_IP_address("h10"), [get_IP_address("h9")], "s4", ["s5"]),
-                **generate_link_entries(get_IP_address("h9"), [get_IP_address("h10")], "s4", ["s1"]),
-            },
-            get_dpid("s5"): {
-                **generate_link_entries(get_IP_address("h10"), [get_IP_address("h9")], "s5", ["h9"]),
-                **generate_link_entries(get_IP_address("h9"), [get_IP_address("h10")], "s5", ["s4"]),
             },
         }
